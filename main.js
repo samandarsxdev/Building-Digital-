acef9fb3507768f3824dd7e2c654ceadc8517296
samandarsxdev <93664571+samandarsxdev@users.noreply.github.com>
2021-11-17 13:00:01 -0500

    add

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// var number = 5;
+// var age= prompt("x ga qiymat bering");
+// var myNumber=x;
+// console.log(age);
+// prompt()=myNumber;
+// var res=5*myNumber; 
+
+// console.log(res);
+
+// var age = prompt("x ga qiymat bering");
+// console.log(age);
+// var number = 6;
+// var res = number * age;
+// console.log(res);
+
+// var age = prompt("x ga qiymat kiriting");
+// // console.log(age);
+// var myNumber = 3*Math.pow(age,3)-6*Math.pow(age,2)-7;
+// console.log(myNumber);
